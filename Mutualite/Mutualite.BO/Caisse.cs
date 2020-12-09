63bd60cff68c6887747ec03c393f9bca7ea07ab2
Mougoue Happi Lionel <72934994+MougoueHappiLionel@users.noreply.github.com>
2020-12-09 06:13:18 +0100

    Serialisation de la classe Caisse, c'est bon!

---
+++ Mutualite/Mutualite.BO/Caisse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutualite.BO
+{
+   public class Caisse
+    {
+        public string NameMem { get; set; }
+        public DateTime DateVersement { get; set; }
+        public int Montant { get; set; }
+
+        public Caisse()
+        {
+
+        }
+
+        public Caisse(string namememb, DateTime datevers, int montant)
+        {
+            NameMem = namememb;
+            DateVersement = datevers;
+            Montant = montant;
+        }
+    }
+}
